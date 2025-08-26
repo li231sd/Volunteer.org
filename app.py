fd63b4e8a7f4410bd88bbac0e1f46fb8b65d1447
SS <113144462+li231sd@users.noreply.github.com>
2025-08-25 20:57:37 -0700

    Quick Fix

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from crypt import methods
 from datetime import datetime
 from math import e
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
 
 import smtplib
 from email.mime.text import MIMEText
